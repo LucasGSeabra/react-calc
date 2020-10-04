3bbe6be84649a9759adbcb9eb56ae742f2ab795f
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-10-04 11:51:57 -0300

    Modificado função clearMemory

--- src/main/Calc.jsx
+++ src/main/Calc.jsx
@@ -24,6 +24,7 @@ export default class Calc extends Component {
 
     clearMemory() {
         console.log('limpar')
+        this.setState({...initialState})
     }
 
     setOperation(operation) {
