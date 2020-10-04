ccc9efc71b363620dc120936f6d5884bfadfdb4e
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-10-04 11:07:11 -0300

    Adicionado checagem de props

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 import './styles.css';
 
 function Button(props) {
+    let classes = 'button ';
+    classes += props.operation ? 'operation' : '';
+    classes += props.double ? 'double' : '';
+    classes += props.triple ? 'triple' : ''
+
     return (
-        <button className="calc-btn">
+        <button 
+            onClick={ e => props.click && props.click(props.label) }
+            className={classes}
+        >
             {props.label}    
         </button>
     )
