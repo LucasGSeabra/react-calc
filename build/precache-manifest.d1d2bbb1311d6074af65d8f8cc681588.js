a39e4d3fb679dd488e99f048c2612683e19889fb
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-10-04 12:56:29 -0300

    Deploy para Github Pages

---
+++ build/precache-manifest.d1d2bbb1311d6074af65d8f8cc681588.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "791d7060244af94245d2e5b4cf5c96c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baa9a84d25609a7f403f",
+    "url": "/static/css/main.84ec8674.chunk.css"
+  },
+  {
+    "revision": "24be7e5acb2efffd105c",
+    "url": "/static/js/2.097449a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.097449a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baa9a84d25609a7f403f",
+    "url": "/static/js/main.353aa96c.chunk.js"
+  },
+  {
+    "revision": "7f57cc3626a1ea002f83",
+    "url": "/static/js/runtime-main.1d20e92e.js"
+  },
+  {
+    "revision": "7cb58857d294ac1e09b72ea9403c690a",
+    "url": "/static/media/RobotoMono-Thin.7cb58857.ttf"
+  }
+]);
