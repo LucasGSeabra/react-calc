9c02019333af99ced9a2537d2116dc99e6dcf58d
Lucas Seabra <lucasgseabra@gmail.com>
2020-10-09 16:16:34 -0300

    Updates

---
+++ precache-manifest.76fe0ffda9e46321fca7f5e7b5e72e90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2d256fdf8be41fbc1b792169ee468a7",
+    "url": "/react-calc/index.html"
+  },
+  {
+    "revision": "b97e4ab6e577dc36bae6",
+    "url": "/react-calc/static/css/main.bbfea06a.chunk.css"
+  },
+  {
+    "revision": "ae81c348695b1f9b6cad",
+    "url": "/react-calc/static/js/2.f2af5646.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calc/static/js/2.f2af5646.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b97e4ab6e577dc36bae6",
+    "url": "/react-calc/static/js/main.eae101d3.chunk.js"
+  },
+  {
+    "revision": "316ae6202d6dd97efa2a",
+    "url": "/react-calc/static/js/runtime-main.ef11baa1.js"
+  },
+  {
+    "revision": "7cb58857d294ac1e09b72ea9403c690a",
+    "url": "/react-calc/static/media/RobotoMono-Thin.7cb58857.ttf"
+  }
+]);
